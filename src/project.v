ce2f6b762e61d5eb30541d2f50d1f52bbe54c851
Filip Bukowski <71112872+BukowskiFilip@users.noreply.github.com>
2024-11-03 17:26:25 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -3,48 +3,47 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none
+`timescale 1ns / 1ps
 
-module tt_um_secB_B15_array_multiplier (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-);
-
-  // All output pins must be assigned. If not used, assign to 0.
-  
-    wire [3:0] m = ui_in[7:4];
-    wire [3:0] q = ui_in[3:0];
-    wire [7:0] p;
-
-    always @(*) begin
-        p = 0; // Initialize product to 0
-        for (integer i = 0; i < 4; i = i + 1) begin
-            if (q[i]) begin
-                p = p + (m << i); // Shift and add
-            end
-        end
-    end
-
-  assign uo_out = p;
-
-
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
+module lab_3_q1b(
+    input [3:0] x,
+    input [3:0] y,
+    input add_sub_select,
+    output [3:0] z,
+    output carry_out
+    );
+    
+wire int_sig1, int_sig2, int_sig3;
+   
+adder instOne (x[0], y[0]^add_sub_select, z[0], int_sig1, add_sub_select);
+adder instTwo (x[1], y[1]^add_sub_select, z[1], int_sig1, add_sub_select);
+adder instThree (x[2], y[2]^add_sub_select, z[2], int_sig1, add_sub_select);
+adder instFour (x[3], y[3]^add_sub_select, z[3], int_sig1, add_sub_select);
 
 endmodule
 
+module adder(
+    input x,
+    input y,
+    output z,
+    output carry_out,
+    input carry
+);
 
-//module array_mult_structural (
-//   input [3:0] m,
-//    input [3:0] q,
-//    output reg [7:0] p
-//);
+wire int_sig1, int_sig2, int_sig3, int_sig4, int_sig5, int_sig6, int_sig7, int_sig8;
+
+assign int_sig1 = x & -y;
+assign int_sig2 = -x & y;
+assign int_sig3 = int_sig1 + int_sig2;
+
+assign int_sig4 = int_sig3 & -carry;
+assign int_sig5 = -int_sig3 & carry;
+assign z = int_sig4 + int_sig5;
+
+assign int_sig6 = x & y;
+assign int_sig7 = y & carry;
+assign int_sig8 = carry & x;
+
+assign carry_out = int_sig6 + int_sig7 + int_sig8;
+
+endmodule
